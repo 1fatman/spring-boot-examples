079b6d166c24602031e35bd16995e4cf9a65bb41
daquan <15197463299@163.com>
2020-02-27 20:51:02 +0800

    commit

--- spring-boot-shiro/src/main/java/com/dq/SpringBootShiroApplication.java
+++ spring-boot-shiro/src/main/java/com/dq/SpringBootShiroApplication.java
@@ -1,4 +1,4 @@
-package com.dq.springbootshiro;
+package com.dq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-shiro/src/test/java/com/dq/SpringBootShiroApplicationTests.java
+++ spring-boot-shiro/src/test/java/com/dq/SpringBootShiroApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dq.springbootshiro;
+package com.dq;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
